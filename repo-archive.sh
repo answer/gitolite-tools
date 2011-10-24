babe4223a518e29fce31859c6e263a2e941556a8
sakai shunsuke <sakai@ans-web.co.jp>
2011-10-24 17:29:43 +0900

    backup, archive の追加

---
+++ repo-archive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ -f ~/.github-tools.rc ] && . ~/.github-tools.rc
+
+if [ -z "$gitolite_repo_path" ]; then
+	gitolite_repo_path=~/repositories
+fi
+if [ -z "$gitolite_archive_path" ]; then
+	gitolite_archive_path=~/.gitolite.archives
+fi
+if [ -z "$gitolite_archive_logrotate_path" ]; then
+	gitolite_archive_logrotate_path=~/gitolite-tools/logrotate.d
+fi
+
+if [ ! -d "$gitolite_archive_path" ]; then
+	mkdir -p "$gitolite_archive_path"
+fi
+if [ ! -d "$gitolite_archive_path" ]; then
+	echo "failed create $gitolite_archive_path"
+	exit
+fi
+
+archive=$gitolite_archive_path/backup.tar.gz
+conf=$gitolite_archive_path/logrotate.conf
+
+echo -n $archive >> $conf
+cat $gitolite_archive_logrotate_path/archive >> $conf
+
+rm -f "$archive" && /bin/tar zcf "$archive" "$gitolite_repo_path" && /usr/sbin/logrotate $conf
